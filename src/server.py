d90755eeb0a6472714cce51da5682acbbc632e4e
Sidahmed <sidahmed.malaoui@gmail.com>
2017-02-24 13:33:07 +0100

    Faster 'combi_partioner' (fixes #2).
     - Made the 'combi_partioner' create combinations faster.
     - Reduced the server usage of the cpu by changing the select timeout (the read() in
             the client stays the same).

--- src/server.py
+++ src/server.py
@@ -46,41 +46,58 @@ def combi_manager(combi_list, client):
 
 	if client in combi_and_client:
 		del combi_and_client[client]
-		combi_and_client[client] = "".join(combi_list[0])
-		del combi_list[0]
-		return combi_and_client[client]
-	else:
-		combi_and_client[client] = "".join(combi_list[0])
-		del combi_list[0]
-		return combi_and_client[client]
+
+	combi_and_client[client] = combi_list[0]
+	del combi_list[0]
+	return combi_and_client[client]
 
 
 def combi_partitioner(combi_list, string_length, per_time): 
-	#Fills the list "combi_list" example : 
-	#per_time = 2 , string_length = 3
-	#combi_list = ["aaa", "aac", "aae", ...]
+	"""Fills the list "combi_list" example : 
+	per_time = 2 , string_length = 3
+	combi_list = ["aaa", "aac", "aae", ...] """ 
 
-	string = ['a'] * string_length
+	string = [ord('a')] * string_length
 
 	total_combin = 26 ** string_length # WILL CHANGE WHEN WE THE MASK #################################################### IMPORTANT
 
-	combi_list.append(list(string))
-
-
 	pbar = tqdm.tqdm(total = total_combin)
 
-	while(1):
+	combi_list.append("".join([chr(c) for c in string]))
 
-		i = 0
-		while(i < per_time):
-			error = next_string_rec(string, string_length -1)
-			if (error == -1):
-				pbar.close()
-				return(-1)
-			i += 1
-		combi_list.append(list(string))
+	# This loop will stop when we see that we are trying to bypass 'z' on the last character of the string. 
+	while True:
+		step = per_time
+		# Uncomment this for debugging.
+		# print("".join([chr(c) for c in string]))
+		# time.sleep(0.1)
+
+		try:
+			while step != 0:
+				# We increment each character with the correct value.
+				for i in range(-1, -string_length - 1, -1):
+					string[i] = string[i] + (step % 26)
+
+					# If we see that the current character value is bypassing 'z', then we fix the current character
+					# and increment the next characters correctly.
+					if string[i] > 122:
+						for j in range(i, -string_length - 1, -1):
+							if string[j] > 122:
+								string[j] = (string[j] % 123) + 97
+								string[j-1] = string[j-1] + 1
+							else:
+								break
+					step //= 26 # We divide the step to go to the next character of the string.
+
+		# If we are trying to increment a character in an index bigger that the string length, then we know 
+		# that we finished generating all the combinations.
+		except IndexError:
+			break
+		combi_list.append("".join([chr(c) for c in string]))
 		pbar.update(per_time)
-	
+
+	return 0
+
 
 def next_string_rec(string, current_case): 
 	#recursive function to calculate the next string 
@@ -106,6 +123,10 @@ def main():
 	Hash = ""
 	combi_list = []
 
+	main_connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	main_connection.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # To refuse the socket if it is in time_wait
+	main_connection.bind(("0.0.0.0", 12345))
+	main_connection.listen(10)
 
 
 	per_time = input("Enter per_time number : ")
@@ -121,7 +142,7 @@ def main():
 	###############################################################
 	main_connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	main_connection.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # To refuse the socket if it is in time_wait
-	main_connection.bind(("0.0.0.0", 12854))
+	main_connection.bind(("0.0.0.0", 12345))
 	main_connection.listen(10)															  #
 	###############################################################
 
@@ -138,7 +159,7 @@ def main():
 	
 	while(1):
 		try:
-			readable, writeable, exceptional = select.select(clients_info,[] ,[] ,0)
+			readable, writeable, exceptional = select.select(clients_info,[] ,[] ,0.01)
 
 			for readable_sock in readable:
 				msg = readable_sock.recv(1024)
