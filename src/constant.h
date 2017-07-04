feac58d4ebfc807a6d86f85e0c8787cb538fcbe9
Sidahmed <sidahmed.malaoui@gmail.com>
2017-07-04 10:08:42 +0100

    Added constants types in 'constant.h' to avoid errors.

--- src/constant.h
+++ src/constant.h
@@ -1,8 +1,8 @@
 #ifndef CONSTANT_H
 #define CONSTANT_H
 
-const READY 	= '0';
-const FOUND 	= '1';
-const NOT_FOUND = '2';
+const char READY 	= '0';
+const char FOUND 	= '1';
+const char NOT_FOUND = '2';
 
 #endif
